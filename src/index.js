dfac63bfec864df24624560e0ca247848a2bd283
LaurenAlayza <65056376+LaurenAlayza@users.noreply.github.com>
2021-02-17 21:32:38 -0500

    Add files via upload
    linked css sheet to html

--- src/index.js
+++ src/index.js
@@ -12,14 +12,3 @@ open.addEventListener('click', () => {
 start.addEventListener('click', () => {
   slideshow_container.classList.add('show');
 });
-
-
-//toggle the modal content from welcome to carousel
-// function toggleModal() {
-//   var modal_container = document.getElementById("modal_container");
-//   if (modal_container).style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
